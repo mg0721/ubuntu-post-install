c5aba7c78f70e4bfba189c5a31ee0b47f32b9f97
mg0721 <64mingn@gmail.com>
2021-07-11 20:50:35 +0900

    111

---
+++ scripts/install_git.sh
@@ -0,0 +1,7 @@
+function git_setting {
+    git config --global user.name $GIT_NAME
+    git config --global user.email $GIT_MAIL
+    # git config --global core.whitespace trailing-space
+    #,-space-before-tab,indent-with-non-tab,tab-in-indent,cr-at-eol
+    git config --list
+}
